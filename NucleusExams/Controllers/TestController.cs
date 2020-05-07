b109789b66b2d0d075f3feb2531caccf3c8feab1
Nilesh61194 <47514664+Nilesh61194@users.noreply.github.com>
2020-05-07 21:01:32 +0530

    Add files via upload

---
+++ NucleusExams/Controllers/TestController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using NucleusExams.Models;
+namespace NucleusExams.Controllers
+{
+    public class TestController : Controller
+    {
+        // GET: Paper
+        
+        public ActionResult Index(decimal examid)
+        {
+            using (dbEntities entities = new dbEntities())
+            {
+                var objQue = entities.EA_QuestionMaster.Where(e => e.ExamID == examid).ToList();
+                return View(objQue);
+            }
+        }
+    }
+}
---
+++ NucleusExams/Controllers/demoController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NucleusExams.Controllers
+{
+    public class demoController : Controller
+    {
+        // GET: demo
+        public string Index()
+        {
+            return "Nilesh Lokhande";
+        }
+    }
+}
